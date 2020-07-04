655dbb7a9ad3fba7fb80a53af609a0c19f3f9efe
madebyfabian <beer.fabian@mail.de>
2020-07-04 09:00:23 +0200

    Few tests with svelte

--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,17 @@
 figma.showUI(__html__, { 
-  width: 400, 
-  height: 300
+  width: 600, 
+  height: 400
 })
 
+
+
+const onSelectionChange = () => {
+	figma.ui.postMessage({
+		type: 'selectionchange',
+		value: figma.currentPage.selection
+	})
+}
+
+onSelectionChange()
+
+figma.on('selectionchange', () => { onSelectionChange() })
