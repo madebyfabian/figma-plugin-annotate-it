560d7a969e7c839e114cd20ee80db0cdf37c84cb
madebyfabian <beer.fabian@mail.de>
2020-07-25 13:20:25 +0200

    Moved tooltip directive into ui.ts

--- src/ui.ts
+++ src/ui.ts
@@ -7,7 +7,30 @@ Vue.config.productionTip = false
 Vue.config.devtools = false
 
 
-import vTooltipDirective from '@/directives/v-tooltip'
+const vTooltipDirective = {
+  inserted: ( el, binding, vnode ) => {
+    const modifiers = Object.keys(binding.modifiers)
+    const modifier = modifiers && modifiers.length ? modifiers[0] : 'bottom-center'
+    
+    let timeout = null
+
+    el.addEventListener('mouseover', () => {
+      timeout = setTimeout(() => {
+        // console.log('v-tooltip - mouseover')
+        el.setAttribute('figma-tooltip-position', modifier)
+        el.setAttribute('figma-tooltip', binding.value)
+      }, 500)
+    })
+
+    el.addEventListener('mouseout', () => {
+      // console.log('v-tooltip - mouseout')
+      el.removeAttribute('figma-tooltip-position')
+      el.removeAttribute('figma-tooltip')
+      clearTimeout(timeout)
+    })
+  }
+}
+
 Vue.directive('tooltip', vTooltipDirective)
 
 
